0dbd331a39373cf5e074ede00c4e95adee1c610a
Brian Flannery <brian@paradigmshiftdev.io>
2026-07-11 07:20:05 -0400

    feat: wire multi-service routes and shared layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Layout from "./components/Layout";
+import LandingPage from "./components/LandingPage";
+import TechnicalConsulting from "./components/TechnicalConsulting";
+import AutomatedWorkflows from "./components/AutomatedWorkflows";
 import DemoPage from "./demos/commercialCleaning/DemoPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/services/technical-consulting"
+          element={<TechnicalConsulting />}
+        />
+        <Route
+          path="/services/automated-workflows"
+          element={<AutomatedWorkflows />}
+        />
+      </Route>
       <Route path="/demos/commercial-cleaning" element={<DemoPage />} />
     </Routes>
   );
