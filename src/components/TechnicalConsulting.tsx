a4221aec5f48fc31c6698f0f0a9551a6342d1a56
Brian Flannery <brian@paradigmshiftdev.io>
2026-07-11 06:09:42 -0400

    feat: add technical consulting service page

---
+++ src/components/TechnicalConsulting.tsx
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import WhoIsFor from "./WhoIsFor";
+import Problem from "./Problem";
+import Offer from "./Offer";
+import HowItWorks from "./HowItWorks";
+import WhyWorkWithMe from "./WhyWorkWithMe";
+import CTA from "./CTA";
+
+const TechnicalConsulting = () => {
+  return (
+    <>
+      <Hero />
+      <WhoIsFor />
+      <Problem />
+      <Offer />
+      <HowItWorks />
+      <WhyWorkWithMe />
+      <CTA />
+    </>
+  );
+};
+
+export default TechnicalConsulting;
